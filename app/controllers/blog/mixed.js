5a37e1f73eebbee0c40057b086f44f0c8bb19ef3
raohl1 <raohl1@lenovo.com>
2017-09-14 21:30:18 +0800

    commit

---
+++ app/controllers/blog/mixed.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+  router = express.Router();
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+router.get('/', function (req, res, next) {
+  res.redirect('/posts');
+});
+
+router.get('/about', function (req, res, next) {
+  res.render('blog/index', {
+    title: 'About Me',
+    posts: posts,
+    pretty: true
+  });
+});
+
+router.get('/contact', function (req, res, next) {
+  res.render('blog/index', {
+    title: 'Contact Me',
+    posts: posts,
+    pretty: true
+  });
+});
---
+++ app/controllers/blog/post.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+  router = express.Router(),
+  mongoose = require('mongoose'),
+  Post = mongoose.model('Post');
+
+module.exports = function (app) {
+  app.use('/posts', router);
+};
+
+router.get('/', function (req, res, next) {
+  Post.find().populate('author').populate('category').exec(function (err, posts) {
+    // return res.json(posts);
+    if (err) return next(err);
+    res.render('blog/index', {
+      posts: posts,
+      pretty: true
+    });
+  });
+});
+
+router.get('/view', function (req, res, next) {
+});
+
+router.get('/comment', function (req, res, next) {
+});
+
+router.get('/favourite', function (req, res, next) {
+});
